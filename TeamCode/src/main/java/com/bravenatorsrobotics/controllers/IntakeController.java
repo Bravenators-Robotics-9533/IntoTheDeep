f21914b2024b32d99b90111156e55e9254d1c152
MGree07 <dubtown45090@gmail.com>
2024-09-29 16:45:08 -0400

    chore: made pincher open less

--- TeamCode/src/main/java/com/bravenatorsrobotics/controllers/IntakeController.java
+++ TeamCode/src/main/java/com/bravenatorsrobotics/controllers/IntakeController.java
@@ -14,8 +14,8 @@ public class IntakeController extends AbstractController {
     public static final double INITIAL_PIVOT_POSITION   = 0;
     public static final double FULL_PIVOT_POSITION      = 0.49;
 
-    public static final double INITIAL_TENSION_POSITION = 0;
-    public static final double FULL_TENSION_POSITION    = 0.5;
+    public static final double INITIAL_TENSION_POSITION = .5;
+    public static final double FULL_TENSION_POSITION    = 1;
 
     protected final IntakeComponent intakeComponent;
 
