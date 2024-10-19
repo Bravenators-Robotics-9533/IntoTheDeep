cec948bf9ea362ec86e83614c230f94bd8dc6cf2
MGree07 <dubtown45090@gmail.com>
2024-10-19 17:13:30 -0400

    Revert "docs: add comments"
    This reverts commit d03b8abd4a7ef464bbf001fe9476f1ac0bd7f5e8.

--- TeamCode/src/main/java/com/bravenatorsrobotics/multicontroller/ScoringMultiController.java
+++ TeamCode/src/main/java/com/bravenatorsrobotics/multicontroller/ScoringMultiController.java
@@ -5,22 +5,8 @@ import com.bravenatorsrobotics.controllers.IntakeController;
 import com.bravenatorsrobotics.controllers.LiftController;
 import com.bravenatorsrobotics.controllers.SwingArmController;
 
-/*
-
-    TODO: READ
-    YOU DID ADD THE MEMBER VARIABLE LIFT_CONTROLLER CORRECTLY THAT WILL WORK.
-    AND ADDING THOSE METHODS BELOW WAS A GOOD IDEA. BUT PLEASE GO THROUGH AND FOLLOW
-    PASSIVE NAMING CONVENTION. EG. GO_TO_START_POSITION SO IT SHOULD ALSO BE
-    GO_TO_GRAB_POSITION NOT GO_TO_GRABBING_POSITION. JUST LITTLE THINGS BUT IT IS IMPORTANT FOR
-    PROPER DOCUMENTATION AND CLIENT EXPECTATION
-
-    BTW. SCORING POSITION IS FINE BUT GRABBING ISN'T
-
-    !!! ALSO PLEASE PUT SPACES BEFORE THE {
-
-
- */
 public class ScoringMultiController extends AbstractMultiController {
+
 
     protected final LiftController liftController;
     protected final IntakeController intakeController;
